a16fa31f28cdc696544144b66c5abb33e6b4d8dd
kaixed <kaixed@users.noreply.github.com>
2023-12-09 15:38:02 +0000

    deploy: kaixed/my-blog@d200a1a186208d98b2da825cad83b2557f716933

--- js/main.js
+++ js/main.js
@@ -270,6 +270,91 @@ let wleelw_musicPlaying = false
 
 let sco = {
     /**
+     * 个性定位
+     */
+    card_welcome: function () {
+        ipLoacation = window.saveToLocal.get('ipLocation');
+        if (!ipLoacation) {
+            // 数据已过期或不存在
+            let script = document.createElement('script');
+            let url = `https://apis.map.qq.com/ws/location/v1/ip?key=${txkey}&output=jsonp`;
+            script.src = url;
+            window.QQmap = function (data) {
+                ipLoacation = data;
+                // 将数据保存到 localStorage，过期时间设置为 1 天
+                window.saveToLocal.set('ipLocation', ipLoacation, 1);
+                document.body.removeChild(script);
+                delete window.QQmap;
+            };
+            document.body.appendChild(script);
+        }
+        showWelcome();
+    },
+    /**
+     * 那年今日
+     */
+    card_history: function () {
+        if (document.getElementById('history-container')) {
+            async function fetchHistoryData() {
+                let myDate = new Date();
+                let myMonth = myDate.getMonth() + 1;
+                let getDate = myDate.getDate();
+                let getMonth = myMonth < 10 ? "0" + myMonth : "" + myMonth;
+                let getDay = getDate < 10 ? "0" + getDate : "" + getDate;
+                let getMonthDate = "S" + getMonth + getDay;
+                let history_data_url = `https://cdn.meuicat.com/gh/Zfour/Butterfly-card-history@2.08/${getMonth}.json`;
+
+                let response = await fetch(history_data_url);
+                let data = await response.json();
+                return data[getMonthDate];
+            }
+
+            function append(parent, text) {
+                let temp = document.createElement('div');
+                temp.innerHTML = text;
+                let frag = document.createDocumentFragment();
+                while (temp.firstChild) {
+                    frag.appendChild(temp.firstChild);
+                }
+                parent.appendChild(frag);
+            }
+
+            fetchHistoryData().then(data => {
+                let html_item = data.map(item => `
+            <div class="swiper-slide history_slide">
+                <span class="history_slide_time">A.D.${item.year}</span>
+                <span class="history_slide_link">${item.title}</span>
+            </div>
+        `).join('');
+                let history_container_wrapper = document.getElementById('history_container_wrapper');
+                append(history_container_wrapper, html_item);
+                let swiper_history = new Swiper('.history_swiper-container', {
+                    passiveListeners: true,
+                    spaceBetween: 30,
+                    effect: 'coverflow',
+                    coverflowEffect: {
+                        rotate: 30,
+                        slideShadows: false,
+                    },
+                    loop: true,
+                    direction: 'vertical',
+                    autoplay: {
+                        disableOnInteraction: true,
+                        delay: 5000
+                    },
+                    mousewheel: false,
+                });
+                let history_container = document.getElementById('history-container');
+                history_container.onmouseenter = function () {
+                    swiper_history.autoplay.stop();
+                };
+                history_container.onmouseleave = function () {
+                    swiper_history.autoplay.start();
+                }
+            });
+        }
+    },
+    /**
      * 隐藏协议小助手
      */
     hideCookie: function () {
@@ -957,7 +1042,7 @@ window.refreshFn = () => {
     GLOBAL_CONFIG.covercolor && coverColor();
     sco.initConsoleState()
     GLOBAL_CONFIG.comment.enable && newestCommentInit() // 最新评论
-    GLOBAL_CONFIG.comment.commentBarrage && initializeCommentBarrage() // 热评
+    GLOBAL_CONFIG.comment.commentBarrage && PAGE_CONFIG.comment && initializeCommentBarrage() // 热评
 }
 
 sco.initTheme()
